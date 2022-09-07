918134c62dcfeb1a91f53d06383b2eb1d9ad7c0e
Ian Jones <ianjones834@protonmail.com>
2022-09-07 16:34:43 -0400

    fixed spacing

--- src/crc-32.js
+++ src/crc-32.js
@@ -3,12 +3,12 @@ const { Bits } = require('../lib/ibyte/src/bits');
 const alg = require('./components/crc-algorithm');
 
 module.exports.checksumFinder = async (filepath) => {
-    let crc = new Bits (0, 32);
-  
-    const stream = fs.createReadStream(filepath, {highWaterMark: 1})
-    for await (const buffer of stream) {
-      crc = alg.crcAlgorithm(crc, buffer);
-    }
+  let crc = new Bits (0, 32);
+  const stream = fs.createReadStream(filepath, {highWaterMark: 1})
+
+  for await (const buffer of stream) {
+    crc = alg.crcAlgorithm(crc, buffer);
+  }
 
    return '0x' + crc.toHex();
 }
